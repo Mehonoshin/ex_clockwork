6bb800c72529ee50aecb9dacd5c79947944e6000
Stanislav Mekhonoshin <Stanislav.Mekhonoshin@dataart.com>
2016-11-17 13:52:02 +0300

    Detect required generators

--- lib/mix/tasks/install.ex
+++ lib/mix/tasks/install.ex
@@ -11,29 +11,36 @@ defmodule Mix.Tasks.ExClockwork.Install do
   @config_marker_end   "%% End ExClockwork Configuration %%"
 
   def run(args) do
-    opts = prepare_options(args)
-    Mix.shell.info "#{inspect opts}"
-  end
-
-  defp install_schedule do
-  end
-
-  defp install_handlers do
-  end
-
-  defp install_config do
+    args
+    |> prepare_options
+    |> Enum.into(%{})
+    |> Enum.each fn
+      {:schedule, true} -> install_schedule
+      {:handlers, true} -> install_handlers
+      {:config, true}   -> install_config
+    end
   end
 
   defp prepare_options(args) do
     {opts, parsed, unknown} = OptionParser.parse(args)
 
-    opts = Enum.reduce(opts, [], fn({key, value}, acc) ->
+    opts = Enum.reduce(opts, @default_options, fn({key, value}, acc) ->
       case Keyword.fetch(@negative_options, key) do
-        {:ok, positive_key} -> acc |> Keyword.put(positive_key, false) |> Keyword.delete(key)
+        {:ok, positive_key} -> acc |> Keyword.delete(positive_key) |> Keyword.delete(key)
         :error -> Keyword.put(acc, key, value)
       end
     end)
+  end
 
-    Keyword.merge(@default_options, opts)
+  defp install_schedule do
+    Mix.shell.info "schedule"
+  end
+
+  defp install_handlers do
+    Mix.shell.info "handlers"
+  end
+
+  defp install_config do
+    Mix.shell.info "config"
   end
 end
